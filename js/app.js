6a4c82e81b81c098808b1a3b3a66412d5d53939f
Abdullah AlBannay <aabannay@gmail.com>
2018-04-11 18:29:46 +0300

    added required variables: allEnimeies, player. Fixed syntax Errors. CANVAS APPEARS NOW

--- js/app.js
+++ js/app.js
@@ -28,24 +28,26 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = functio(){
+var Player = function() {
     this.sprite = 'images/char-boy.png';
     this.x= 200;
     this.y= 200;
     this.speed = 200;
-}
+};
 
 Player.prototype.update = function(dt) {
     this.x = this.speed * dt;
-}
+};
 
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [];
 
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
